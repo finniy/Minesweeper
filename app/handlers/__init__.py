45ea551094786041e2498d37ba1a2e8c6f291839
anton <anton.lakhtion@gmail.com>
2025-08-19 16:41:26 +0300

    Add __init__.py

---
+++ app/handlers/__init__.py
@@ -0,0 +1,6 @@
+import app.handlers.game
+import app.handlers.home
+import app.handlers.profile
+import app.handlers.rating
+import app.handlers.rules
+import app.handlers.start
---
+++ app/utils/__init__.py
@@ -0,0 +1,5 @@
+import app.utils.board
+import app.utils.found_scores
+import app.utils.game_logic
+import app.utils.key
+import app.utils.keyboards
